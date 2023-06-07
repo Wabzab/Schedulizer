918c845ba1c5568bf7461f3b63742445c7407baa
Max Paterson-Jones <100119585+Wabzab@users.noreply.github.com>
2023-06-07 20:50:13 +0200

    DARKMODE!!!

--- app/src/main/java/com/example/schedulizer/SettingsFragment.kt
+++ app/src/main/java/com/example/schedulizer/SettingsFragment.kt
@@ -4,27 +4,20 @@ import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import android.widget.FrameLayout
+import android.widget.Switch
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 
 class SettingsFragment : Fragment(R.layout.fragment_settings) {
 
-    class MainActivity : AppCompatActivity() {
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            setContentView(R.layout.fragment_settings)
-            val frameLayout: FrameLayout = findViewById(R.id.SettingPage)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val frameLayout: FrameLayout = view.findViewById(R.id.SettingPage)
+        val switchDarkMode: Switch = view.findViewById(R.id.switchDarkMode)
+
+        switchDarkMode.setOnClickListener {
             frameLayout.setBackgroundColor(Color.DKGRAY)
         }
-
-        fun onSwitchClick(view: View) {
-            //    enable Dark Mode - disabled since it gives errors
-            //    ConstraintLayout someLayout=(ConstraintLayout)view.findViewById(R.id.someLayout);
-            //    someLayout.getBackground().setColorFilter(Color.parseColor("#1C1B1A"), PorterDuff.Mode.SRC_ATOP);
-            setContentView(R.layout.fragment_settings)
-            val frameLayout: FrameLayout = findViewById(R.id.SettingPage)
-            frameLayout.setBackgroundColor(Color.DKGRAY)
-        }
-
     }
 }
