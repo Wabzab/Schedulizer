aae4debec9e8888b6af1193fad74151a8016a3a0
Max Ospovat <ST10082045@vcconnect.edu.za>
2023-06-07 08:09:07 +0200

    Settings Page UI designed

--- app/src/main/java/com/example/schedulizer/SettingsFragment.kt
+++ app/src/main/java/com/example/schedulizer/SettingsFragment.kt
@@ -1,7 +1,14 @@
 package com.example.schedulizer
 
+import android.view.View
+import android.widget.FrameLayout
 import androidx.fragment.app.Fragment
 
 class SettingsFragment : Fragment(R.layout.fragment_settings) {
+    fun onSwitchClick(view: View) {
+        //    enable Dark Mode - disabled since it gives errors
+        //    ConstraintLayout someLayout=(ConstraintLayout)view.findViewById(R.id.someLayout);
+        //    someLayout.getBackground().setColorFilter(Color.parseColor("#1C1B1A"), PorterDuff.Mode.SRC_ATOP);
+    }
 
 }
