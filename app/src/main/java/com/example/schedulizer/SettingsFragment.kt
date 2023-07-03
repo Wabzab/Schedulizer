2bcdabc6f3990228c06376015e8627ab104a1f19
Max Ospovat <ST10082045@vcconnect.edu.za>
2023-07-03 20:41:03 +0200

    Added Min and Max hour goals Per Day

--- app/src/main/java/com/example/schedulizer/SettingsFragment.kt
+++ app/src/main/java/com/example/schedulizer/SettingsFragment.kt
@@ -9,11 +9,18 @@ import android.widget.Switch
 import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
 import android.content.Context
-
+import android.widget.Button
+import android.widget.TextView
+import kotlin.math.min
 
 
 class SettingsFragment : Fragment(R.layout.fragment_settings) {
-
+lateinit var btnMinPlus: Button
+lateinit var btnMinMinus: Button
+lateinit var btnMaxPlus: Button
+lateinit var btnMaxMinus: Button
+lateinit var txtMinText: TextView
+lateinit var txtMaxText: TextView
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -21,12 +28,38 @@ class SettingsFragment : Fragment(R.layout.fragment_settings) {
         val switchDarkMode: Switch = view.findViewById(R.id.switchDarkMode)
         val switchMuteSound: Switch = view.findViewById(R.id.switchMuteSound)
         var isButtonMute: Boolean = true
+        var minHours = 2
+        var maxHours = 4
+        btnMinPlus = view.findViewById(R.id.btnMinPlus)
+        btnMinMinus = view.findViewById(R.id.btnMinMinus)
+        btnMaxPlus = view.findViewById(R.id.btnMaxPlus)
+        btnMaxMinus = view.findViewById(R.id.btnMaxMinus)
+        txtMinText = view.findViewById(R.id.txtMinText)
+        txtMaxText = view.findViewById(R.id.txtMaxText)
         lateinit var audioManager: AudioManager
+
         audioManager = context?.getSystemService(Context.AUDIO_SERVICE) as AudioManager
 
 
         switchDarkMode.setOnClickListener {
             frameLayout.setBackgroundColor(Color.DKGRAY)
+        }
+
+        btnMinPlus.setOnClickListener {
+            minHours = minHours + 1
+           txtMinText.text = "Min: $minHours"
+        }
+        btnMinMinus.setOnClickListener {
+            minHours = minHours - 1
+            txtMinText.text = "Min: $minHours"
+        }
+        btnMaxPlus.setOnClickListener {
+            maxHours = maxHours + 1
+            txtMaxText.text = "Max: $maxHours"
+        }
+        btnMaxMinus.setOnClickListener {
+            maxHours = maxHours - 1
+            txtMaxText.text = "Max: $maxHours"
         }
 
         switchMuteSound.setOnClickListener {
