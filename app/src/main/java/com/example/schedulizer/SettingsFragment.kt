f117df7842ab935ca8b798bb059fdaea857377f7
Max Ospovat <ST10082045@vcconnect.edu.za>
2023-07-03 19:37:47 +0200

    Added functionality for Mute All Sounds button

--- app/src/main/java/com/example/schedulizer/SettingsFragment.kt
+++ app/src/main/java/com/example/schedulizer/SettingsFragment.kt
@@ -1,12 +1,16 @@
 package com.example.schedulizer
 
 import android.graphics.Color
+import android.media.AudioManager
 import android.os.Bundle
 import android.view.View
 import android.widget.FrameLayout
 import android.widget.Switch
-import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
+import android.content.Context
+
+
 
 class SettingsFragment : Fragment(R.layout.fragment_settings) {
 
@@ -15,9 +19,73 @@ class SettingsFragment : Fragment(R.layout.fragment_settings) {
 
         val frameLayout: FrameLayout = view.findViewById(R.id.SettingPage)
         val switchDarkMode: Switch = view.findViewById(R.id.switchDarkMode)
+        val switchMuteSound: Switch = view.findViewById(R.id.switchMuteSound)
+        var isButtonMute: Boolean = true
+        lateinit var audioManager: AudioManager
+        audioManager = context?.getSystemService(Context.AUDIO_SERVICE) as AudioManager
+
 
         switchDarkMode.setOnClickListener {
             frameLayout.setBackgroundColor(Color.DKGRAY)
         }
+
+        switchMuteSound.setOnClickListener {
+            if (isButtonMute) {
+                audioManager.adjustStreamVolume(
+                    AudioManager.STREAM_NOTIFICATION,
+                    AudioManager.ADJUST_MUTE,
+                    0
+                );
+                audioManager.adjustStreamVolume(
+                    AudioManager.STREAM_ALARM,
+                    AudioManager.ADJUST_MUTE,
+                    0
+                );
+                audioManager.adjustStreamVolume(
+                    AudioManager.STREAM_MUSIC,
+                    AudioManager.ADJUST_MUTE,
+                    0
+                );
+                audioManager.adjustStreamVolume(
+                    AudioManager.STREAM_RING,
+                    AudioManager.ADJUST_MUTE,
+                    0
+                );
+                audioManager.adjustStreamVolume(
+                    AudioManager.STREAM_SYSTEM,
+                    AudioManager.ADJUST_MUTE,
+                    0
+                );
+                isButtonMute = false
+            } else {
+                audioManager.adjustStreamVolume(
+                    AudioManager.STREAM_NOTIFICATION,
+                    AudioManager.ADJUST_UNMUTE,
+                    0
+                );
+                audioManager.adjustStreamVolume(
+                    AudioManager.STREAM_ALARM,
+                    AudioManager.ADJUST_UNMUTE,
+                    0
+                );
+                audioManager.adjustStreamVolume(
+                    AudioManager.STREAM_MUSIC,
+                    AudioManager.ADJUST_UNMUTE,
+                    0
+                );
+                audioManager.adjustStreamVolume(
+                    AudioManager.STREAM_RING,
+                    AudioManager.ADJUST_UNMUTE,
+                    0
+                );
+                audioManager.adjustStreamVolume(
+                    AudioManager.STREAM_SYSTEM,
+                    AudioManager.ADJUST_UNMUTE,
+                    0
+                );
+                isButtonMute = true
+            }
+        }
+
     }
 }
